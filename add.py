59e1080b5d6589343975e904661438488920a6a9
tamperic <tamaraperic.work@gmail.com>
2025-07-28 18:55:13 +0200

    Add requirements.txt and add.py file

---
+++ add.py
@@ -0,0 +1,6 @@
+first_num_input = int(input("Enter the first number: "))
+second_num_input = int(input("Enter the second number: "))
+
+result_num = first_num_input + second_num_input
+
+print(first_num_input, " + ", second_num_input, " = ", result_num)
